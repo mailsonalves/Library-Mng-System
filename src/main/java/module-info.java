c3f1dc0e7087a107c183105ac1cc2555b972b0e1
mailsonalves <mailsonalves.new@gmail.com>
2023-12-29 11:28:29 -0300

    feat: add mvc pattern

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module com.example.demo {
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
+    exports com.example.demo.controller;
+    opens com.example.demo.controller to javafx.fxml;
 }
